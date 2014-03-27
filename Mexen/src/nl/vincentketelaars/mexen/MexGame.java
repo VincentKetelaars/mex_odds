58640b87fc149cd38e36722c93faa36a97de09a8
Vincent Ketelaars <vincentketelaars@gmail.com>
2014-03-27 19:24:53 +0100

    MexGame opts between Mexen and Blind Mexen. The RollDice activity merely throws two dice. Title is adjusted to the game. Can return home by app icon.

--- Mexen/src/nl/vincentketelaars/mexen/MexGame.java
+++ Mexen/src/nl/vincentketelaars/mexen/MexGame.java
@@ -1,65 +1,33 @@
 package nl.vincentketelaars.mexen;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MexGame extends Activity {
+public class MexGame extends Activity implements OnClickListener {
+	
+	Button mexButton; 
+	Button blindMexButton; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mex_game);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+		setContentView(R.layout.activity_mex_game);
         
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mex_game, menu);
-        return true;
+        mexButton = (Button) findViewById(R.id.mex_button);
+        mexButton.setOnClickListener(this);
+        blindMexButton = (Button) findViewById(R.id.blind_mex_button);
+        blindMexButton.setOnClickListener(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_mex_game, container, false);
-            return rootView;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, RollDice.class);
+		intent.putExtra("game", v.getId());
+		startActivity(intent);
+	}
 
 }
